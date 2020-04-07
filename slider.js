12e545eab41f01fece367c3460b58bf5442f503b
Ursula Kesek <ursky@blueyonder.co.uk>
2020-04-07 13:47:31 +0100

    edited slider

--- slider.js
+++ slider.js
@@ -24,3 +24,22 @@ function showSlides(n) {
     }
     slides[slideIndex -1].style.display = "block";
 }
+/*automatic slide show
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 1500); // Change image every 1.5 seconds
+}*/
